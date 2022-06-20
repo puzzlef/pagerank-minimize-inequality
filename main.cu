f851161ee6aff04cad7572645ce2f2d4593d2b58
Subhajit Sahu <wolfram77@gmail.com>
2022-06-21 00:24:46 +0530

    :mango: fix basic errors

--- main.cu
+++ main.cu
@@ -21,21 +21,23 @@ void runExperiment(const G& x, int batch) {
   auto lc = lorenzCurve(a.ranks);
   auto gc = giniCoefficient(lc);
   printf("[%05d edges; %.10f gini_coef.] original\n", 0, gc);
+  auto y  = duplicate(x);
+  auto yt = duplicate(xt);
   for (int i=0; i<batch; i++) {
-    auto [u, v] = edgeInsertLowRank(x, a.ranks);
-    x.addEdge(u, v);  x.correct();
-    xt.addEdge(v, u); xt.correct();
-    auto b  = pagerankMonolithicCuda(x, xt, init);
+    auto [u, v] = edgeInsertLowRank(y, a.ranks);
+    y.addEdge(u, v);  y.correct();
+    yt.addEdge(v, u); yt.correct();
+    auto b  = pagerankMonolithicCuda(y, yt, &a.ranks);
     auto lc = lorenzCurve(b.ranks);
     auto gc = giniCoefficient(lc);
-    printf("[%05d edges; %.10f gini_coef.] edgeInsertLowRank\n", b+1, gc);
+    printf("[%05d edges; %.10f gini_coef.] edgeInsertLowRank\n", i+1, gc);
   }
 }
 
 
 int main(int argc, char **argv) {
   char *file = argv[1];
-  int batch = argc>2? stoi(argv[2]) : 1000;
+  int batch = argc>2? stoi(argv[2]) : 100;
   printf("Loading graph %s ...\n", file);
   auto x  = readMtxOutDiGraph(file); println(x);
   selfLoopW(x, [](auto u) { return true; }); print(x); printf(" (selfLoopAllVertices)\n");
--- src/heuristics.hxx
+++ src/heuristics.hxx
@@ -8,27 +8,36 @@ using std::make_pair;
 
 
 
-// Pick source vertex with high contrib.
-template <class G, class T>
-auto vertexHighContrib(const G& x, const vector<T>& r) {
+// Pick vertex with high vertex property.
+template <class G, class F>
+auto vertexHighBy(const G& x, F fn) {
   using K = typename G::key_type;
+  using T = decltype(fn(K()));
   T max  = T();
   K maxu = K();
   x.forEachVertexKey([&](auto u) {
-    T val = r[u] / (x.degree(u) + 1);
+    T val = fn(u);
     if (val > max) { max = val; maxu = u; }
   });
   return maxu;
 }
 
+// Pick vertex with high contrib.
+template <class G, class T>
+auto vertexHighContrib(const G& x, const vector<T>& r) {
+  auto fn = [&](auto u) { return r[u] / (x.degree(u) + 1); };
+  return vertexHighBy(x, fn);
+}
+
+
+
 
 // Generate insertion edge pointing to low vertex property.
-template <class G, class F>
-auto edgeInsertLowBy(const G& x, F fn) {
+template <class G, class T, class F>
+auto edgeInsertLowBy(const G& x, const vector<T>& r, F fn) {
   using K = typename G::key_type;
-  using T = decltype(fn(K()));
   T min  = T(1);
-  T minv = K();
+  K minv = K();
   K u = vertexHighContrib(x, r);
   x.forEachVertexKey([&](auto v) {
     T val = fn(u);
@@ -36,35 +45,35 @@ auto edgeInsertLowBy(const G& x, F fn) {
     if (x.hasEdge(u, v)) return;
     min = val; minv = v;
   });
-  return make_pair(u, v);
+  return make_pair(u, minv);
 }
 
 // Generate insertion edge pointing to low rank.
 template <class G, class T>
 auto edgeInsertLowRank(const G& x, const vector<T>& r) {
   auto fn = [&](auto v) { return r[v]; };
-  return edgeInsertLowBy(x, fn);
+  return edgeInsertLowBy(x, r, fn);
 }
 
 // Generate insertion edge pointing to low contrib.
 template <class G, class T>
 auto edgeInsertLowContrib(const G& x, const vector<T>& r) {
   auto fn = [&](auto v) { return r[v] / (x.degree(v) + 1); };
-  return edgeInsertLowBy(x, fn);
+  return edgeInsertLowBy(x, r, fn);
 }
 
 // Generate insertion edge pointing to low rank and high reverse rank.
 template <class G, class T>
 auto edgeInsertLowRankHighReverse(const G& x, const vector<T>& r, const vector<T>& s) {
   auto fn = [&](auto v) { return r[v] * (1 - s[v]); };
-  return edgeInsertLowBy(x, fn);
+  return edgeInsertLowBy(x, r, fn);
 }
 
 // Generate insertion edge pointing to low contrib and high reverse rank.
 template <class G, class T>
 auto edgeInsertLowContribHighReverse(const G& x, const vector<T>& r, const vector<T>& s) {
   auto fn = [&](auto v) { return r[v] * (1 - s[v]) / (x.degree(v) + 1); };
-  return edgeInsertLowBy(x, fn);
+  return edgeInsertLowBy(x, r, fn);
 }
 // edgeInsertLowRankHighRevrank
 // edgeInsertLowRankHighRevcontrib
